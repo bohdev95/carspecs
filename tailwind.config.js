cb0e17845814462413c86e2b4c8feb8e26112038
bohdev95 <boleinikov95@gmail.com>
2022-11-07 08:34:07 -0500

    update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-slate-200": "#fafafa",
+      },
+    },
+  },
+  plugins: [],
+};
